604c37b3177def062247d01cbe58c5a8219b6d13
Leon Markovich <leonmarkovich97@gmail.com>
2024-07-14 22:44:13 +0300

    fixed linter and ruff formating

--- python_ask_service/backend/app.py
+++ python_ask_service/backend/app.py
@@ -3,27 +3,30 @@ import openai
 from flask import request, jsonify
 import os
 from dotenv import load_dotenv
+
 app = create_app()
 load_dotenv()
 client = openai.OpenAI(
-    api_key = os.environ.get("OPENAI_API_KEY"),
+    api_key=os.environ.get("OPENAI_API_KEY"),
 )
+
+
 @app.get("/ping")
 def hello():
     return "pong!"
 
+
 @app.post("/ask")
 def ask_question():
-    data=request.json
-    question = data.get('question')
+    data = request.json
+    question = data.get("question")
     if not question:
-        return jsonify({'error': 'No question provided'}), 400
+        return jsonify({"error": "No question provided"}), 400
     try:
-         response = client.chat.completions.create(
-        model="gpt-3.5-turbo",
-        messages=[{"role": "user", "content": question}]
-    )
-         answer = response.choices[0].message.content.strip()
-         return jsonify({'answer': answer})
+        response = client.chat.completions.create(
+            model="gpt-3.5-turbo", messages=[{"role": "user", "content": question}]
+        )
+        answer = response.choices[0].message.content.strip()
+        return jsonify({"answer": answer})
     except Exception as e:
-        return jsonify({'error': str(e)}), 500
+        return jsonify({"error": str(e)}), 500
